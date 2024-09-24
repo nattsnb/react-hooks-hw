0f43a05be528f2f15a8e40a016a0836bb2e12a36
Natasza ASzatan <natiye.nb@gmai.com>
2024-09-24 21:09:39 +0200

    after lesson

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useState, useEffect, useCallback, useMemo } from 'react';
+
+function useFetchedUsers() {
+    const [users, setUsers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((data) => {
+                setUsers(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    }, []);
+
+    return {
+        users,
+        isLoading,
+    }
+}
+
+const UsersList = () => {
+    const { isLoading, users } = useFetchedUsers();
+
+    return (
+        <div>
+            <h1>Users List</h1>
+            {
+                isLoading && <p>Loading...</p>
+            }
+            {!isLoading && (
+                <ul>
+                    {users.map((user) => (
+                        <li key={user.id}>{user.name}</li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    );
+}
+
+export const App = () => {
+    return <UsersList />
+}
